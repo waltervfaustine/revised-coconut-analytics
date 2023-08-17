fe580fffbe2a407491797fe223b827b820e1d0ed
Walter V. Faustine <waltervfaustine@gmail.com>
2023-08-17 16:46:46 +0300

    feat: 🎸 added support for deployment shell script for github
    This commit will add support for deployment shell script for github

---
+++ _attachments/docker-build-image-github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run 'git pull' to update the repository
+git pull
+
+# Copy the 'config.json' file
+sudo cp /home/serveruser/coconut-implementation/coconut-config/config.json .
+
+# Build the Docker containers using docker-compose
+sudo docker-compose build
