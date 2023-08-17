88d017cb47114cc5a8842df30f55689f62c80a85
Walter V. Faustine <waltervfaustine@gmail.com>
2023-08-17 13:40:30 +0300

    feat: 🎸 added support for shell script for building docker imag
    This commit will add support for shell script for building docker image

---
+++ _attachments/docker-build-analytics.sh
@@ -0,0 +1,3 @@
+# Build a Docker image named 'udsm-dhis2-lab/coconut-analytics' with the tag '1.0.0'
+# The '.' at the end specifies that the build context is the current directory
+docker build -t udsm-dhis2-lab/coconut-analytics:1.0.0 .
